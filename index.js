7b2dcdb32b372373584c64ecad785895b44da79f
Antonio Guerriere <antonioguerriere@gmail.com>
2021-07-26 16:30:03 +0100

    middleware test

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 const express = require('express');
 const path = require('path');
+const logger = require('./middleware/logger');
+const members = require('./Members');
 
 const app = express();
 
-// Send one static page
-// app.get('/', (req, res) => {
-//     res.sendFile(path.join(__dirname, 'public', 'index.html'))
-// });
+// Init middleware
+// app.use(logger);
+
+// Gets all Members
+app.get('/api/members', (req, res) => res.json(members));
 
 // Set static folder
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, 'public')));
 
 const PORT = process.env.PORT || 5000;
 
